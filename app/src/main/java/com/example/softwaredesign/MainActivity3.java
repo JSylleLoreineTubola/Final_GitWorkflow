8f78e24cdbc91615af68e571061d94fef511f633
RosheilLylaEscobar <79897887+RosheilLylaEscobar@users.noreply.github.com>
2021-04-13 02:16:37 +0800

    Update

--- app/src/main/java/com/example/softwaredesign/MainActivity3.java
+++ app/src/main/java/com/example/softwaredesign/MainActivity3.java
@@ -14,7 +14,7 @@ public class MainActivity3 extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main3);
 
        q1 = (Button) findViewById(R.id.q1);
        q1.setOnClickListener(new View.OnClickListener() {
--- app/src/main/java/com/example/softwaredesign/bible.java
+++ app/src/main/java/com/example/softwaredesign/bible.java
@@ -1,14 +1,23 @@
 package com.example.softwaredesign;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
 
 import android.os.Bundle;
 
 public class bible extends AppCompatActivity {
 
+    ViewPager viewPager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bible);
+
+        viewPager = findViewById(R.id.viewPager);
+
+        IntroAdapter adapter = new IntroAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapter);
     }
 }
